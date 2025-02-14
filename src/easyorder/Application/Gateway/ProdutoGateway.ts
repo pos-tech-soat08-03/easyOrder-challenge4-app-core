0fbbf32bbb1a1334e166fb35aa2f4fd508649758
Marcio Saragiotto <marcio.saragiotto@gmail.com>
2025-02-14 16:54:40 -0300

    fix: ajuste chamada e endpoint pedido

--- src/easyorder/Application/Gateway/ProdutoGateway.ts
+++ src/easyorder/Application/Gateway/ProdutoGateway.ts
@@ -3,6 +3,7 @@ import { ProdutoEntity } from "../../Core/Entity/ProdutoEntity";
 import { CategoriaEnum } from "../../Core/Entity/ValueObject/CategoriaEnum";
 import { ProdutoGatewayInterface } from "../../Core/Interfaces/Gateway/ProdutoGatewayInterface";
 import { DataNotFoundException } from "../../Core/Types/ExceptionType";
+import { collapseTextChangeRangesAcrossMultipleVersions } from "typescript";
 export class ProdutoGateway implements ProdutoGatewayInterface {
   private readonly apiUrl: string;
 
@@ -12,21 +13,28 @@ export class ProdutoGateway implements ProdutoGatewayInterface {
 
   public async buscarProdutoPorId(id: string): Promise<ProdutoEntity | null> {
     try {
-      const response = await axios.get(`${this.apiUrl}/produtos/buscar/${id}`);
-
-      if (!response.data) {
-        throw new DataNotFoundException("Produto não encontrado");
+      console.log(`Buscando produto no Microserviço de Produtos - id ${id}`);
+      const chamada_produto = `http://${this.apiUrl}/produto/buscar/${id}`;
+      console.log(`Chamada: ${chamada_produto}`);
+      const response = await axios.get(chamada_produto).catch((error) => {
+        console.error(`Erro ao acessar microserviço: ${error.message}`);
+        throw new DataNotFoundException("Não foi possível acessar o microserviço de produtos");
+      });
+      if (!response || !response.data) {
+        throw new DataNotFoundException("Erro obtendo dados do microserviço de produtos");
       }
+      console.log(`Produto encontrado: ${response.data}`);
 
       return new ProdutoEntity(
-        response.data.nome,
-        response.data.descricao,
-        response.data.preco,
-        response.data.categoria as CategoriaEnum,
-        response.data.imagemURL,
-        response.data.id
+        response.data.produto.nome,
+        response.data.produto.descricao,
+        response.data.produto.preco,
+        response.data.produto.categoria as CategoriaEnum,
+        response.data.produto.imagemURL,
+        response.data.produto.id
       );
-    } catch (error) {
+    } catch (error: any) {
+      console.error(`Erro ao buscar produto: ${error.message}`);
       throw error;
     }
   }
